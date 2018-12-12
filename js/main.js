1619d4140a60b66bc7180354d27ea352741d7407
Andrey Parshin <jobandrux@yandex.ru>
2018-12-12 21:38:27 +0300

    121218_h1

---
+++ js/main.js
@@ -0,0 +1,49 @@
+window.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+
+  //Tabs
+  let classTab = document.querySelectorAll('.decoration_item'),
+    classWrapTab = document.querySelectorAll('.decoration_slider')[0],
+    classTabContentItem = document.querySelectorAll('.content_tab'),
+    classTabActive = classWrapTab.querySelectorAll('.decoration_item > div');
+  // console.log(classTabActive)
+
+  function hideContent(num) {
+    for (let i = num; i < classTabContentItem.length; i++) {
+      classTabContentItem[i].classList.remove('show');
+      classTabContentItem[i].classList.add('hide');
+
+    }
+  }
+
+  function unsetActivTab(num) {
+    for (let i = 0; i < classTabActive.length; i++) {
+        classTabActive[i].classList.remove('after_click');
+      }  
+     classTabActive[num].classList.add('after_click');
+  }
+
+  function showContent(num) {
+    hideContent(0);
+    classTabContentItem[num].classList.add('show');
+  }
+
+  function tabClick() {
+    classWrapTab.addEventListener('click', (event) => {
+      let target = event.target;
+      if (target && target) {
+        for (let i = 0; i < classTab.length; i++) {
+          if (target && (classWrapTab.getElementsByTagName('a')[i] == target || classWrapTab.getElementsByClassName('no_click')[i] == target)) {
+            hideContent(0);
+            showContent(i);
+            unsetActivTab(i);
+            break;
+          }
+        }
+      }
+    })
+  }
+
+  hideContent(1);
+  tabClick();
+})
